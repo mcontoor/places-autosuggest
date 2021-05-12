b94b7c64c65958b2633f71202ffc49b0a6353955
Meghna Contoor <mcontoor@gmail.com>
2021-05-12 18:34:49 +0530

    add map box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 let autoComplete;
 
@@ -27,7 +28,6 @@ function handleScriptLoad(updateQuery, updateGeometry,  autoCompleteRef) {
     autoCompleteRef.current,
     { componentRestrictions: { country: "in" } }
   );
-  console.log(autoComplete)
   autoComplete.setFields(["address_components", "formatted_address", "geometry"]);
   autoComplete.addListener("place_changed", () =>
     handlePlaceSelect(updateQuery, updateGeometry)
@@ -42,10 +42,40 @@ async function handlePlaceSelect(updateQuery, updateGeometry) {
   updateGeometry(geometry);
 }
 
+const containerStyle = {
+  width: '400px',
+  height: '300px'
+};
+
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 function App() {
   const [address, updateAddress] = useState('');
   const [geometry, updateGeometry] = useState({});
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  })
   const autoCompleteRef = useRef(null);
+  const [map, setMap] = useState(null)
+  const [lat, updateLat] = useState(0);
+  const [lng, updateLng] = useState(180);
+  
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((pos) => {updateLng(pos.coords.longitude); updateLng(pos.coords.latitude)}, (err) => console.log('err', err), options);
+  // }, []);
+
+  useEffect(() => {
+    if(geometry.lat && geometry.lng) {
+    updateLat(parseFloat(geometry?.lat?.() || 0));
+    updateLng(parseFloat(geometry?.lng?.() || 180));
+    }
+  }, [geometry])
 
   useEffect(() => {
     loadScript(
@@ -53,6 +83,16 @@ function App() {
       () => handleScriptLoad(updateAddress, updateGeometry, autoCompleteRef)
     );
   }, []);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map?.fitBounds?.(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
 
   return (
     <div className="App">
@@ -66,9 +106,32 @@ function App() {
           onChange={(e) => updateAddress(e.target.value)}
           style={{width: '100%'}}
           placeholder="Enter anything"/>
-          <p>Latitude: {geometry?.lat?.()}</p> 
-          <p>Longitude: {geometry?.lng?.()}</p>
+          <p>Latitude: {lat}</p> 
+          <p>Longitude: {lng}</p>
         </div>
+        {
+          (isLoaded) ? (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={{
+                lat: lat || 0,
+                lng: lng || 180,
+              }}
+              zoom={18}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+              onCenterChanged={() => {
+                if(!!map) {
+                  updateLat(map.center.lat());
+                  updateLng(map.center.lng());
+                }
+              }}
+            >
+              { /* Child components, such as markers, info windows, etc. */ }
+              {lat && lng && <Marker position={{ lat, lng }} />}
+            </GoogleMap>
+        ) : <></>
+        }
       </header>
     </div>
   );
